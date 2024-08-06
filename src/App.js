96d220da3a30343a8bc2acbbfb8164b3885d6453
Rishitabhatt <rishtabhatt@gmail.com>
2024-08-06 13:50:40 +0530

    add footer section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import './App.css';
 import Contnet from './components/Contnet';
 import Banner from './components/Banner';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
    <Header/>
 <Contnet/>
 <Banner/>
+<Footer/>
    </>
   );
 }
